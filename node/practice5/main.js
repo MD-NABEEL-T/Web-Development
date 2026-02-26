47b921e290bae630fbc251e3886b40179576bfdf
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2026-02-26 06:48:13 +0530

    alright

---
+++ node/practice5/main.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+//with the help of this we can access the files inside public dir
+
+// app.get or app.post or app.post or app.delete(path,handler)
+app.get('/', (req, res) => {
+  res.send('I Am Iron Man World!')
+})
+
+app.get('/about', (req, res) => {
+  res.send('About')
+})
+
+app.get('/contact', (req, res) => {
+  res.send('Contact us!')
+})
+
+app.get('/blog/:slug', (req, res) => {
+  //logic to fetch {slug } from the db
+  res.send(`hello ${req.params.slug}`)
+})
+
+app.get('/blog', (req, res) => {
+  res.send('blog about me')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
