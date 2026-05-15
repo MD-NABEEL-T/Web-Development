df9fd33a9bd3bcb90d045a0ec692894b129590b5
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2026-05-15 07:16:43 +0530

    learnt ejs && git push origin main

--- node/practice9/server.js
+++ node/practice9/server.js
@@ -2,10 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.sendFile('/templates/index.html', { root: __dirname })
+  let siteName="Adidas"
+  let searchText="lorem ipsum"
+  // res.sendFile('/templates/index.html', { root: __dirname })
+  res.render("index",{siteName:siteName,searchText:searchText})
 })
 
+app.get('/blog/:slug',(req,res)=>{
+  let blogTitle ="My brand"
+  let blogContent = "lorem23"
+  res.render("templates/blogpost.html",{blogTitle:blogTitle,blogContent:blogContent})
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
